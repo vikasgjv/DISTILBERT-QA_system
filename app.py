10104e0031fe2a8addcd6bc1973cde0ca2d65297
Vikass <vikasgjv@gmail.com>
2026-07-02 17:01:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ model.eval()
 print("Model Loaded Successfully!")
 
 
-
 def qa_model(question, context):
     """Run QA inference and return answer with confidence score."""
     inputs = tokenizer(
